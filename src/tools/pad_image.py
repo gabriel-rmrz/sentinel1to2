ac20eb120765a3196f6a06531d1ac7321224630b
Gabriel Ramirez <garamire@dhcp217.ba.infn.it>
2025-10-29 15:07:22 +0100

    NOT RUNNING VERSION: changing structure of directories, adding config file and testing argument parsing

--- src/tools/pad_image.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-def pad_image(img, patch_size, stride):
-  c, h, w = img.shape
-  pad_h = (stride - h % stride) % stride
-  pad_w = (stride - w % stride) % stride
-  padded = np.pad(img, ((0, 0), (0, pad_h), (0, pad_w)), mode='reflect')
-  return padded, h, w
